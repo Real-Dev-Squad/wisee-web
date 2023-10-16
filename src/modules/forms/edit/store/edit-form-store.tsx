1cb1740a5a2bf8b955622e129cacc03589d4ad81
Yash Raj <yash.raj.i@pagarbook.com>
2023-10-17 02:08:06 +0530

    Remove comment

--- src/modules/forms/edit/store/edit-form-store.tsx
+++ src/modules/forms/edit/store/edit-form-store.tsx
@@ -5,7 +5,6 @@ import { BlockTypeEnum } from "../enums"
 import { TBlock } from "../types/edit-form-types"
 
 const DEFAULT_BLOCKS: TBlock[] = [
-    // Using nanoId here causes an issue as the id becomes different on server and client
     { id: "form_title", payload: { data: '', placeholder: '' }, type: BlockTypeEnum.FORM_TITLE },
 ]
 
