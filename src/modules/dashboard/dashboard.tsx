3fd23fffdc7d4e960f5476e8efe7abb566ecfaa6
yash raj <yashraj0207@outlook.com>
2024-03-05 22:56:01 +0530

    add title to dashboard

--- src/modules/dashboard/dashboard.tsx
+++ src/modules/dashboard/dashboard.tsx
@@ -1,4 +1,5 @@
 import { Plus } from "lucide-react"
+import Head from "next/head"
 import Link from "next/link"
 
 import { Button, Input } from "@/components"
@@ -61,27 +62,33 @@ const FormCard = () => {
 
 export const Dashboard = () => {
     return (
-        <main className="bg-gray-50 w-ful h-screen flex">
-            <div className="w-60 h-full border-r border-gray-200">
-                <Sidebar />
-            </div>
+        <>
+            <Head>
+                <title>Dashboard - {process.env.NEXT_PUBLIC_APP_NAME}</title>
+            </Head>
 
-            <section className="bg-white h-full flex-1 w-full overflow-auto px-4">
-                <Header className="pt-4 pb-5" />
-
-                <div className="px-4 pb-16 space-y-1">
-                    {new Array(5).fill(0).map((_, i) => <FormCard key={i} />)}
+            <main className="bg-gray-50 w-ful h-screen flex">
+                <div className="w-60 h-full border-r border-gray-200">
+                    <Sidebar />
                 </div>
 
-                <div className="px-6">
-                    <Button asChild variant="ghost" size="lg" className="px-4 [&>svg]:w-7 [&>svg]:h-7">
-                        <Link href="/create" className="flex items-center gap-2 text-2xl text-gray-300 hover:text-gray-400 transition hover:bg-gray-100 py-2 px-3 w-max rounded-lg">
-                            <Plus />
-                            <span className="font-medium">Create Form</span>
-                        </Link>
-                    </Button>
-                </div>
-            </section>
-        </main>
+                <section className="bg-white h-full flex-1 w-full overflow-auto px-4">
+                    <Header className="pt-4 pb-5" />
+
+                    <div className="px-4 pb-16 space-y-1">
+                        {new Array(5).fill(0).map((_, i) => <FormCard key={i} />)}
+                    </div>
+
+                    <div className="px-6">
+                        <Button asChild variant="ghost" size="lg" className="px-4 [&>svg]:w-7 [&>svg]:h-7">
+                            <Link href="/create" className="flex items-center gap-2 text-2xl text-gray-300 hover:text-gray-400 transition hover:bg-gray-100 py-2 px-3 w-max rounded-lg">
+                                <Plus />
+                                <span className="font-medium">Create Form</span>
+                            </Link>
+                        </Button>
+                    </div>
+                </section>
+            </main>
+        </>
     )
 }
