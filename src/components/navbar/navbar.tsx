c7649ea097c5d0c09d1b0bcaf1c827bfcf23496a
yash raj <yashraj0207@outlook.com>
2024-03-12 18:38:14 +0530

    make navbar responsive for tablet view

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -15,8 +15,10 @@ const NavbarLink = ({ href, title, isActive }: NavbarLinkProps) => {
         <Link
             href={href}
             className={cn(
-                "block rounded-full border border-transparent px-3 py-1 text-stone-500 transition hover:bg-stone-200 hover:text-stone-700 xl:rounded",
-                isActive && "border-stone-50 bg-white text-stone-800 shadow shadow-stone-200"
+                "block rounded-full border border-transparent px-3 py-1 text-stone-500 transition hover:bg-stone-100 hover:text-stone-700",
+                "lg:rounded-lg lg:shadow-none",
+                isActive &&
+                    "border-stone-50 bg-white font-medium text-stone-800 shadow shadow-stone-200 hover:bg-stone-100 hover:text-stone-800 lg:bg-stone-100"
             )}
         >
             {title}
@@ -29,12 +31,12 @@ export const Navbar = () => {
     const appName = process.env.NEXT_PUBLIC_APP_NAME
 
     return (
-        <nav className="fixed bottom-2 left-0 w-full border-none border-stone-200 p-4 xl:h-full xl:w-60 xl:border-r xl:px-2">
-            <h1 className="hidden px-2 pb-6 text-2xl font-semibold text-stone-900 xl:block">{appName}</h1>
+        <nav className="fixed bottom-2 left-0 w-full p-4 lg:relative lg:h-full lg:w-56 lg:border-r lg:border-stone-200  lg:px-2 xl:w-60">
+            <h1 className="hidden px-2 py-4 text-2xl font-semibold text-stone-900 lg:block">{appName}</h1>
 
-            <ul className="mx-auto flex w-max justify-center gap-2 rounded-full bg-stone-100 px-2 py-1.5 xl:w-full xl:flex-col">
+            <ul className="mx-auto flex w-max justify-center gap-2 rounded-full bg-stone-100 px-2 py-1.5 lg:w-full lg:flex-col lg:bg-transparent">
                 {NAVBAR_LINKS.map((link, index) => (
-                    <li key={index} className="w-max xl:w-full">
+                    <li key={index} className="w-max lg:w-full">
                         <NavbarLink title={link.title} href={link.href} isActive={pathname.startsWith(link.href)} />
                     </li>
                 ))}
--- src/modules/dashboard/dashboard.tsx
+++ src/modules/dashboard/dashboard.tsx
@@ -14,31 +14,20 @@ export const Dashboard = () => {
                 <title>Dashboard - {process.env.NEXT_PUBLIC_APP_NAME}</title>
             </Head>
 
-            <main className="w-ful flex h-screen bg-stone-50">
+            <main className="w-ful flex h-screen">
                 <Navbar />
 
                 <section className="h-full w-full flex-1 overflow-auto bg-white px-4">
                     <DashboardHeader className="pb-5 pt-4" />
 
                     <div className="space-y-1 px-4 pb-16">
-                        {new Array(5).fill(0).map((_, i) => (
-                            <FormCard
-                                key={i}
-                                id={2}
-                                status="draft"
-                                title="hello"
-                                updated_at="12hours ago"
-                            />
+                        {new Array(10).fill(0).map((_, i) => (
+                            <FormCard key={i} id={2} status="draft" title="hello" updated_at="12hours ago" />
                         ))}
                     </div>
 
                     <div className="px-6">
-                        <Button
-                            asChild
-                            variant="ghost"
-                            size="lg"
-                            className="px-4 text-2xl [&>svg]:h-7 [&>svg]:w-7"
-                        >
+                        <Button asChild variant="ghost" size="lg" className="px-4 text-2xl [&>svg]:h-7 [&>svg]:w-7">
                             <Link
                                 href="/create"
                                 className="flex w-max items-center gap-2 rounded-lg px-3 py-2 text-stone-300 transition hover:bg-stone-100 hover:text-stone-400"
