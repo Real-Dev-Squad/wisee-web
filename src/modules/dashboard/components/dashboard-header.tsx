d706db1a75f200f6431572ff34a67f76eafae2c5
yash raj <yashraj0207@outlook.com>
2024-03-12 18:47:13 +0530

    make button responsive for mobile and add app name to dashboard header for mobile

--- src/modules/dashboard/components/dashboard-header.tsx
+++ src/modules/dashboard/components/dashboard-header.tsx
@@ -8,11 +8,16 @@ type Props = {
 }
 
 export const DashboardHeader = ({ className }: Props) => {
+    const appName = process.env.NEXT_PUBLIC_APP_NAME
+
     return (
-        <div data-testid="dashboard_header" className={cn("flex items-center justify-end gap-4", className)}>
-            <Input placeholder="Search" className="w-72" />
+        <div data-testid="dashboard_header" className={cn("flex items-center justify-end gap-2 lg:gap-4", className)}>
+            <h1 className="pr-8 text-2xl font-semibold text-stone-900 lg:hidden">{appName}</h1>
+
+            <Input placeholder="Search" className="h-8 w-full lg:h-9 lg:w-72" />
+
             <Button size="sm" className="shrink-0">
-                <Plus /> Create Form
+                <Plus /> <span className="hidden lg:inline-block">Create Form</span>
             </Button>
         </div>
     )
