3fcd7d6d5d8e55bb6b0e4c86824103cbadf800db
yash raj <yashraj0207@outlook.com>
2024-03-12 18:19:17 +0530

    make navbar responsive for mobile

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,15 +1,23 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
+
+import { cn } from "@/utils/classname"
 
 import { NAVBAR_LINKS } from "./navbar.constants"
 import { TNavbarLink } from "./navbar.types"
 
-type NavbarLinkProps = TNavbarLink
+type NavbarLinkProps = TNavbarLink & {
+    isActive: boolean
+}
 
-const NavbarLink = ({ href, title }: NavbarLinkProps) => {
+const NavbarLink = ({ href, title, isActive }: NavbarLinkProps) => {
     return (
         <Link
             href={href}
-            className="block rounded px-3 py-1 text-stone-500 transition hover:bg-stone-200 hover:text-stone-700"
+            className={cn(
+                "block rounded-full border border-transparent px-3 py-1 text-stone-500 transition hover:bg-stone-200 hover:text-stone-700 xl:rounded",
+                isActive && "border-stone-50 bg-white text-stone-800 shadow shadow-stone-200"
+            )}
         >
             {title}
         </Link>
@@ -17,16 +25,17 @@ const NavbarLink = ({ href, title }: NavbarLinkProps) => {
 }
 
 export const Navbar = () => {
+    const { pathname } = useRouter()
     const appName = process.env.NEXT_PUBLIC_APP_NAME
 
     return (
-        <nav className="fixed bottom-0 left-0 h-full w-full border-r border-stone-200 px-2 py-4 xl:w-60">
-            <h1 className="px-2 pb-6 text-2xl font-semibold text-stone-900">{appName}</h1>
+        <nav className="fixed bottom-2 left-0 w-full border-none border-stone-200 p-4 xl:h-full xl:w-60 xl:border-r xl:px-2">
+            <h1 className="hidden px-2 pb-6 text-2xl font-semibold text-stone-900 xl:block">{appName}</h1>
 
-            <ul className="w-full space-y-2">
+            <ul className="mx-auto flex w-max justify-center gap-2 rounded-full bg-stone-100 px-2 py-1.5 xl:w-full xl:flex-col">
                 {NAVBAR_LINKS.map((link, index) => (
-                    <li key={index} className="w-full">
-                        <NavbarLink title={link.title} href={link.href} />
+                    <li key={index} className="w-max xl:w-full">
+                        <NavbarLink title={link.title} href={link.href} isActive={pathname.startsWith(link.href)} />
                     </li>
                 ))}
             </ul>
