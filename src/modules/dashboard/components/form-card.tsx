67d21235f8a8ee778def6635e63fa5d7d5661b3a
yash raj <yashraj0207@outlook.com>
2024-03-12 18:54:55 +0530

    make form card responsive for mobile

--- src/modules/dashboard/components/form-card.tsx
+++ src/modules/dashboard/components/form-card.tsx
@@ -4,22 +4,30 @@ import { TForm } from "@/api/form-builder/form-builder.types"
 import { ROUTES } from "@/routes"
 import { formatRoute } from "@/utils/format-route/format-route.util"
 
-type Props = Pick<TForm, "id" | 'status' | 'updated_at'> & {
+type Props = Pick<TForm, "id" | "status" | "updated_at"> & {
     title: string // TODO: @yesyash - add this to api response
 }
 
 export const FormCard = ({ id, title, status, updated_at }: Props) => {
     return (
-        <div data-testid="form_card" className="space-y-1 py-5 px-6 hover:bg-stone-100 transition rounded-lg cursor-default">
-            <div className="flex items-center gap-4">
-                <Link href={formatRoute(ROUTES.forms.edit, { id })} className="text-2xl text-stone-950 font-semibold">
+        <div
+            data-testid="form_card"
+            className="cursor-default space-y-1 rounded-lg py-2 transition lg:px-6 lg:py-4 lg:hover:bg-stone-100"
+        >
+            <div className="flex items-center gap-2 lg:gap-4">
+                <Link
+                    href={formatRoute(ROUTES.forms.edit, { id })}
+                    className="text-xl font-semibold text-stone-950 lg:text-2xl"
+                >
                     {title}
                 </Link>
 
-                <span className="uppercase text-xs text-stone-500 rounded bg-stone-100 font-semibold px-2 py-1">{status}</span>
+                <span className="rounded bg-stone-100 px-1 py-0.5 text-xs font-semibold  uppercase text-stone-500 lg:px-2 lg:py-1">
+                    {status}
+                </span>
             </div>
 
-            <p className="text-stone-400 text-sm">{updated_at}</p>
+            <p className="text-sm text-stone-400">{updated_at}</p>
         </div>
     )
 }
--- src/modules/dashboard/dashboard.tsx
+++ src/modules/dashboard/dashboard.tsx
@@ -17,16 +17,16 @@ export const Dashboard = () => {
             <main className="w-ful flex h-screen">
                 <Navbar />
 
-                <section className="h-full w-full flex-1 overflow-auto bg-white px-4">
-                    <DashboardHeader className="pb-5 pt-4" />
+                <section className="h-full w-full flex-1 overflow-auto bg-white px-6 pb-24">
+                    <DashboardHeader className="pb-8 pt-4" />
 
-                    <div className="space-y-1 px-4 pb-16">
+                    <div className="space-y-2 pb-8 lg:space-y-1 lg:px-4 lg:pb-12">
                         {new Array(10).fill(0).map((_, i) => (
                             <FormCard key={i} id={2} status="draft" title="hello" updated_at="12hours ago" />
                         ))}
                     </div>
 
-                    <div className="px-6">
+                    <div className="-mx-4 lg:mx-0 lg:px-6">
                         <Button asChild variant="ghost" size="lg" className="px-4 text-2xl [&>svg]:h-7 [&>svg]:w-7">
                             <Link
                                 href="/create"
